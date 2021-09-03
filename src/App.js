6be1f34629b3a9a82db1d558c80d02620f2a21b7
jay <jay.patel.1@hotmail.com>
2021-09-03 16:36:41 +0530

    resume

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import ReactGA from 'react-ga';
+import $ from 'jquery';
+import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Resume from './Components/Resume';
+import Testimonials from './Components/Testimonials';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      foo: 'bar',
+      resumeData: {}
+    };
+
+    ReactGA.initialize('UA-110570651-1');
+    ReactGA.pageview(window.location.pathname);
+
+  }
+
+  getResumeData(){
+    $.ajax({
+      url:'/resumeData.json',
+      dataType:'json',
+      cache: false,
+      success: function(data){
+        this.setState({resumeData: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount(){
+    this.getResumeData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header data={this.state.resumeData.main}/>
+        <Resume data={this.state.resumeData.resume}/>
+        <Testimonials data={this.state.resumeData.testimonials}/>
+        <Footer data={this.state.resumeData.main}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Resume.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+class Resume extends Component {
+  render() {
+
+    if(this.props.data){
+     
+      var education = this.props.data.education.map(function(education){
+        return <div key={education.school}><h3>{education.school}</h3>
+        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
+        <p>{education.description}</p></div>
+      })
+    
+      var skills = this.props.data.skills.map(function(skills){
+        var className = 'bar-expand '+skills.name.toLowerCase();
+        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
+      })
+    }
+
+    return (
+      <section id="resume">
+
+      <div className="row education">
+         <div className="three columns header-col">
+            <h1><span>Education</span></h1>
+         </div>
+
+         <div className="nine columns main-col">
+            <div className="row item">
+               <div className="twelve columns">
+                 {education}
+               </div>
+            </div>
+         </div>
+      </div>
+
+
+
+      <div className="row skill">
+
+         <div className="three columns header-col">
+            <h1><span>Skills</span></h1>
+         </div>
+
+         <div className="nine columns main-col">
+
+            
+
+				<div className="bars">
+				   <ul className="skills">
+					  {skills}
+					</ul>
+				</div>
+			</div>
+      </div>
+   </section>
+    );
+  }
+}
+
+export default Resume;
